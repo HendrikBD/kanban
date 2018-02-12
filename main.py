c64e0762e5b3824e8163aed0454bd2a322c04b44
HendrikBD <hendrik.danen@gmail.com>
2018-02-11 23:47:14 -0500

    Adding init function and specifying db commands

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ class Kanban():
 
     cols = []
 
+    def __init__(self):
+        print('hello?')
+        #  Load list from db to local list
+
     def list(self):
         for col in eval("self.cols"):
             print(col)
@@ -12,14 +16,19 @@ class Kanban():
     def addCol(self, colName):
         exec("self." + colName + "=self.column()")
         self.cols.append(colName)
+        #  Add new category table
 
     def addItem(self, colName, itemName):
         exec("self."+colName+".items.append('"+itemName+"')")
+        #  Add items to db
+        #  Should items be referenced relatively? Or should each category have
+        #  its own table of a list?
 
     def removeItem(self, colNum, itemNum):
         print(eval("self." + self.cols[colNum-1] + ".items[" + str(itemNum-1) +
                    "]"))
         del eval("self." + self.cols[colNum-1] + ".items")[itemNum-1]
+        #  Remove item from list
 
     def moveItem(self, colFrom, itemNum, colTo):
         self.addItem(self.cols[colTo-1], eval("self." + self.cols[colFrom-1] +
@@ -27,6 +36,7 @@ class Kanban():
                                               "]"))
         self.removeItem(colFrom, itemNum)
         self.list()
+        #  Change db location OR reference table
 
     class column():
 
@@ -37,6 +47,8 @@ class Kanban():
 
 
 def main():
+    #  Seed db suggested structure
+    #  Load kanban & list items
     kan = Kanban()
     kan.addCol("ToDo")
     kan.addCol("Next")
