bb83f1c64bddab33a6f76c9736fa0b58a1401dd6
HendrikBD <hendrik.danen@gmail.com>
2018-02-14 19:36:27 -0500

    Adding the option to add, remove or advance an item

--- cli.py
+++ cli.py
@@ -3,6 +3,16 @@ from main import Kanban
 
 
 @click.command()
-def cli():
+@click.option('--advance', '-v', nargs=2, type=int)
+@click.option('--remove', '-r', nargs=2, type=int)
+@click.option('--add', '-a', nargs=1, type=str)
+def cli(advance, remove, add):
     kan = Kanban()
+    if(advance):
+        kan.advItem(advance[0], advance[1])
+    elif(remove):
+        kan.removeItem(remove[0], remove[1])
+    elif(add):
+        kan.addItem(1, add)
+
     click.echo(kan.printOut())
